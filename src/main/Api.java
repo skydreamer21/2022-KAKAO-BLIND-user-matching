c6d6485624e2910ea79b2f451227b19d5b7261e4
skydreamer21 <skydreamer210@gmail.com>
2022-10-07 13:55:12 +0900

    [Refactor] api 관련 함수들 분리

---
+++ src/main/Api.java
@@ -0,0 +1,142 @@
+package main;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.HashMap;
+
+import util.Util;
+
+public class Api {
+    static Util utils = new Util();
+    static final String AUTH_TOKEN = "3ebf8579866e4e88c49da86d2a8b94fe";
+    static final String BASE_URL = "https://huqeyhi95c.execute-api.ap-northeast-2.amazonaws.com/prod";
+    static final String GET = "GET";
+    static final String POST = "POST";
+    static final String PUT = "PUT";
+
+    public JSONObject startAPI(int problem) throws IOException, ParseException {
+        URL url = new URL(BASE_URL + "/start");
+        HttpsURLConnection httpsConn = (HttpsURLConnection) url.openConnection();
+
+        httpsConn.setRequestMethod(POST);
+        httpsConn.setRequestProperty("X-Auth-Token", AUTH_TOKEN);
+        httpsConn.setRequestProperty("Content-Type", "application/json");
+        httpsConn.setConnectTimeout(1000);
+        httpsConn.setReadTimeout(1000);
+        httpsConn.setDoOutput(true);
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("problem", problem);
+        JSONObject data = new JSONObject(map);
+
+        OutputStream os = httpsConn.getOutputStream();
+        byte[] input = data.toString().getBytes("utf-8");
+        os.write(input, 0, input.length);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(httpsConn.getInputStream(),"utf-8"));
+        StringBuilder response = new StringBuilder();
+        String responseLine = null;
+        while ((responseLine = br.readLine()) != null) {
+            response.append(responseLine.trim());
+        }
+        httpsConn.disconnect();
+
+        JSONParser jsonParser = new JSONParser();
+        return (JSONObject) jsonParser.parse(response.toString());
+    }
+
+    public JSONObject getInfoAPI(String AUTH_KEY, String apiName) throws IOException, ParseException {
+        URL url = new URL(BASE_URL + "/" + apiName);
+        HttpsURLConnection httpsConn = (HttpsURLConnection) url.openConnection();
+        httpsConn.setRequestMethod(GET);
+        httpsConn.setRequestProperty("Authorization", AUTH_KEY);
+        httpsConn.setRequestProperty("Content-Type", "application/json");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(httpsConn.getInputStream(),"utf-8"));
+        StringBuilder response = new StringBuilder();
+        String responseLine = null;
+        while ((responseLine = br.readLine()) != null) {
+            response.append(responseLine.trim());
+        }
+
+        httpsConn.disconnect();
+
+        JSONParser jsonParser = new JSONParser();
+        return (JSONObject) jsonParser.parse(response.toString());
+    }
+
+    public JSONObject matchAPI(String AUTH_KEY, int[][] pairs) throws IOException, ParseException {
+        URL url = new URL(BASE_URL + "/match");
+        HttpsURLConnection httpsConn = (HttpsURLConnection) url.openConnection();
+
+        httpsConn.setRequestMethod(PUT);
+        httpsConn.setRequestProperty("Authorization", AUTH_KEY);
+        httpsConn.setRequestProperty("Content-Type", "application/json");
+        httpsConn.setConnectTimeout(1000);
+        httpsConn.setReadTimeout(1000);
+        httpsConn.setDoOutput(true);
+
+        HashMap<String, JSONArray> map = new HashMap<>();
+        JSONArray pairsJSON = utils.pairsToJSONArray(pairs);
+        map.put("pairs", pairsJSON);
+        JSONObject data = new JSONObject(map);
+//        System.out.println(data.toString());
+
+        OutputStream os = httpsConn.getOutputStream();
+        byte[] input = data.toString().getBytes("utf-8");
+        os.write(input, 0, input.length);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(httpsConn.getInputStream(),"utf-8"));
+        StringBuilder response = new StringBuilder();
+        String responseLine = null;
+        while ((responseLine = br.readLine()) != null) {
+            response.append(responseLine.trim());
+        }
+        httpsConn.disconnect();
+
+        JSONParser jsonParser = new JSONParser();
+        return (JSONObject) jsonParser.parse(response.toString());
+    }
+
+    public JSONObject changeGradeAPI(String AUTH_KEY, HashMap<Integer, Integer> commands) throws IOException, ParseException {
+        URL url = new URL(BASE_URL + "/change_grade");
+        HttpsURLConnection httpsConn = (HttpsURLConnection) url.openConnection();
+
+        httpsConn.setRequestMethod(PUT);
+        httpsConn.setRequestProperty("Authorization", AUTH_KEY);
+        httpsConn.setRequestProperty("Content-Type", "application/json");
+        httpsConn.setConnectTimeout(1000);
+        httpsConn.setReadTimeout(1000);
+        httpsConn.setDoOutput(true);
+
+        HashMap<String, JSONArray> map = new HashMap<>();
+        JSONArray pairsJSON = utils.commandsToJSONArray(commands);
+        map.put("commands", pairsJSON);
+        JSONObject data = new JSONObject(map);
+//        System.out.println(data.toString());
+
+        OutputStream os = httpsConn.getOutputStream();
+        byte[] input = data.toString().getBytes("utf-8");
+        os.write(input, 0, input.length);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(httpsConn.getInputStream(),"utf-8"));
+        StringBuilder response = new StringBuilder();
+        String responseLine = null;
+        while ((responseLine = br.readLine()) != null) {
+            response.append(responseLine.trim());
+        }
+        httpsConn.disconnect();
+
+        JSONParser jsonParser = new JSONParser();
+        return (JSONObject) jsonParser.parse(response.toString());
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,23 @@
 package main;
 
-import java.io.*;
-import java.lang.reflect.Method;
-import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
-import java.net.MalformedURLException;
 
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import util.Util;
 import org.json.simple.JSONObject;
 import java.util.HashMap;
+
+import util.Util;
+import main.Api;
 
 
 public class Main {
     static HttpsURLConnection httpsConn;
     static Util utils = new Util();
+    static Api APIs = new Api();
     static String AUTH_KEY;
+    static int time = 0; // matchAPI 호출 시 1씩 증가
+    static HashMap<Integer, Integer> waitingList = new HashMap<>();
+    static HashMap<Integer, Integer> userGrade = new HashMap<>();
+
     static final String AUTH_TOKEN = "3ebf8579866e4e88c49da86d2a8b94fe";
     static final String BASE_URL = "https://huqeyhi95c.execute-api.ap-northeast-2.amazonaws.com/prod";
     static final String GET = "GET";
@@ -37,148 +37,49 @@ public class Main {
 
     public static void main(String[] argv) throws Exception{
 
-        JSONObject start = startAPI(1);
+        JSONObject start = APIs.startAPI(1);
         AUTH_KEY = (String) start.get("auth_key");
         int time = (int) ((long) start.get("time"));
         System.out.printf("authKey : %s, time : %d\n", AUTH_KEY, time);
 
-        JSONObject userInfo = getInfoAPI(USER_INFO);
+        JSONObject userInfo = APIs.getInfoAPI(AUTH_KEY, USER_INFO);
         System.out.println(userInfo.toString());
 
-        JSONObject match1 = matchAPI(new int[][] {});
+        JSONObject match1 = APIs.matchAPI(AUTH_KEY, new int[][] {});
         System.out.println(match1.toString());
 
-        JSONObject waiting = getInfoAPI(WAITING_LINE);
+        JSONObject waiting = APIs.getInfoAPI(AUTH_KEY, WAITING_LINE);
         System.out.println(waiting.toString());
+
+        JSONObject match2 = APIs.matchAPI(AUTH_KEY, new int[][] {{21,22}, {23,24}, {25,26}, {27,28}, {29,30}});
+        System.out.println(match2.toString());
+
+        JSONObject match3 = APIs.matchAPI(AUTH_KEY, new int[][] {{1,2}, {3,4}, {5,6}, {7,8}, {9,10}, {11, 12}, {13,14}, {15,16}, {17,18}, {19,20}});
+        System.out.println(match3.toString());
+
+        JSONObject gameResult = APIs.getInfoAPI(AUTH_KEY, GAME_RESULT);
+        System.out.println(gameResult.toString());
+
+        for (int i=0; i<45; i++) {
+            JSONObject match_res = APIs.matchAPI(AUTH_KEY, new int[][] {});
+            System.out.println(COLOR_INFO + match_res.toString());
+            System.out.println(COLOR_INFO + APIs.getInfoAPI(AUTH_KEY, GAME_RESULT).toString());
+            System.out.println(COLOR_DEBUG + APIs.getInfoAPI(AUTH_KEY, WAITING_LINE).toString());
+            System.out.println();
+        }
+
+        JSONObject waiting2 = APIs.getInfoAPI(AUTH_KEY, WAITING_LINE);
+        System.out.println(waiting2.toString());
 
         HashMap<Integer, Integer> commands = new HashMap<>();
         commands.put(1, 1000);
         commands.put(2, 1500);
         commands.put(3, 1123);
 
-        JSONObject change = changeGradeAPI(commands);
+        JSONObject change = APIs.changeGradeAPI(AUTH_KEY, commands);
         System.out.println(change.toString());
 
-        JSONObject userInfo2 = getInfoAPI(USER_INFO);
+        JSONObject userInfo2 = APIs.getInfoAPI(AUTH_KEY, USER_INFO);
         System.out.println(COLOR_INFO + userInfo2.toString());
     }
-
-    public static JSONObject startAPI(int problem) throws IOException, ParseException {
-        URL url = new URL(BASE_URL + "/start");
-        HttpsURLConnection httpsConn = (HttpsURLConnection) url.openConnection();
-
-        httpsConn.setRequestMethod(POST);
-        httpsConn.setRequestProperty("X-Auth-Token", AUTH_TOKEN);
-        httpsConn.setRequestProperty("Content-Type", "application/json");
-        httpsConn.setConnectTimeout(1000);
-        httpsConn.setReadTimeout(1000);
-        httpsConn.setDoOutput(true);
-
-        HashMap<String, Integer> map = new HashMap<>();
-        map.put("problem", problem);
-        JSONObject data = new JSONObject(map);
-
-        OutputStream os = httpsConn.getOutputStream();
-        byte[] input = data.toString().getBytes("utf-8");
-        os.write(input, 0, input.length);
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(httpsConn.getInputStream(),"utf-8"));
-        StringBuilder response = new StringBuilder();
-        String responseLine = null;
-        while ((responseLine = br.readLine()) != null) {
-            response.append(responseLine.trim());
-        }
-        httpsConn.disconnect();
-
-        JSONParser jsonParser = new JSONParser();
-        return (JSONObject) jsonParser.parse(response.toString());
-    }
-
-    public static JSONObject getInfoAPI(String apiName) throws IOException, ParseException {
-        URL url = new URL(BASE_URL + "/" + apiName);
-        HttpsURLConnection httpsConn = (HttpsURLConnection) url.openConnection();
-        httpsConn.setRequestMethod(GET);
-        httpsConn.setRequestProperty("Authorization", AUTH_KEY);
-        httpsConn.setRequestProperty("Content-Type", "application/json");
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(httpsConn.getInputStream(),"utf-8"));
-        StringBuilder response = new StringBuilder();
-        String responseLine = null;
-        while ((responseLine = br.readLine()) != null) {
-            response.append(responseLine.trim());
-        }
-
-        httpsConn.disconnect();
-
-        JSONParser jsonParser = new JSONParser();
-        return (JSONObject) jsonParser.parse(response.toString());
-    }
-
-    public static JSONObject matchAPI(int[][] pairs) throws IOException, ParseException {
-        URL url = new URL(BASE_URL + "/match");
-        HttpsURLConnection httpsConn = (HttpsURLConnection) url.openConnection();
-
-        httpsConn.setRequestMethod(PUT);
-        httpsConn.setRequestProperty("Authorization", AUTH_KEY);
-        httpsConn.setRequestProperty("Content-Type", "application/json");
-        httpsConn.setConnectTimeout(1000);
-        httpsConn.setReadTimeout(1000);
-        httpsConn.setDoOutput(true);
-
-        HashMap<String, JSONArray> map = new HashMap<>();
-        JSONArray pairsJSON = utils.pairsToJSONArray(pairs);
-        map.put("pairs", pairsJSON);
-        JSONObject data = new JSONObject(map);
-//        System.out.println(data.toString());
-
-        OutputStream os = httpsConn.getOutputStream();
-        byte[] input = data.toString().getBytes("utf-8");
-        os.write(input, 0, input.length);
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(httpsConn.getInputStream(),"utf-8"));
-        StringBuilder response = new StringBuilder();
-        String responseLine = null;
-        while ((responseLine = br.readLine()) != null) {
-            response.append(responseLine.trim());
-        }
-        httpsConn.disconnect();
-
-        JSONParser jsonParser = new JSONParser();
-        return (JSONObject) jsonParser.parse(response.toString());
-    }
-
-    public static JSONObject changeGradeAPI(HashMap<Integer, Integer> commands) throws IOException, ParseException {
-        URL url = new URL(BASE_URL + "/change_grade");
-        HttpsURLConnection httpsConn = (HttpsURLConnection) url.openConnection();
-
-        httpsConn.setRequestMethod(PUT);
-        httpsConn.setRequestProperty("Authorization", AUTH_KEY);
-        httpsConn.setRequestProperty("Content-Type", "application/json");
-        httpsConn.setConnectTimeout(1000);
-        httpsConn.setReadTimeout(1000);
-        httpsConn.setDoOutput(true);
-
-        HashMap<String, JSONArray> map = new HashMap<>();
-        JSONArray pairsJSON = utils.commandsToJSONArray(commands);
-        map.put("commands", pairsJSON);
-        JSONObject data = new JSONObject(map);
-//        System.out.println(data.toString());
-
-        OutputStream os = httpsConn.getOutputStream();
-        byte[] input = data.toString().getBytes("utf-8");
-        os.write(input, 0, input.length);
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(httpsConn.getInputStream(),"utf-8"));
-        StringBuilder response = new StringBuilder();
-        String responseLine = null;
-        while ((responseLine = br.readLine()) != null) {
-            response.append(responseLine.trim());
-        }
-        httpsConn.disconnect();
-
-        JSONParser jsonParser = new JSONParser();
-        return (JSONObject) jsonParser.parse(response.toString());
-    }
-
-
 }
